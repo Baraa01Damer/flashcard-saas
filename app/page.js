7e6baf67a501e7c3a08092e96cf6c26d1f41ecae
Baraa Damer <baraa01damer@gmail.com>
2024-11-26 21:55:35 -0800

    working on page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
 import Image from 'next/image'
 import getStripe from './utils/get-stripe'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import { AppBar, Button, Container, Toolbar, Typography } from '@mui/material'
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Grid2,
+  Toolbar,
+  Typography
+} from '@mui/material'
 import Head from 'next/head'
 
 export default function Home() {
@@ -14,7 +22,9 @@ export default function Home() {
 
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>Flashcard SaaS</Typography>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
           <SignedOut>
             <Button color="inherit">Login</Button>
             <Button color="inherit">Sign Up</Button>
@@ -24,6 +34,96 @@ export default function Home() {
           </SignedIn>
         </Toolbar>
       </AppBar>
+
+      <Box
+        sx={{
+          textAlign: 'center',
+          my: 4,
+        }}
+      >
+        <Typography variant="h2">Welcome to Flashcard SaaS</Typography>
+        <Typography variant="h5">
+          {' '}
+          The easiest way to make flashcards from your text
+        </Typography>
+        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+          Get Started
+        </Button>
+      </Box>
+      <Box sx={{ my: 6 }}>
+        <Typography variant="h4">Features</Typography>
+        <Grid2 container spacing={4}>
+          <Grid2 item xs={12} md={4}>
+            <Typography variant="h6">Easy Text Input</Typography>
+            <Typography>
+              {' '}
+              Simply input your text and let our software do the rest. Creating
+              flashcards has never been easier.
+            </Typography>
+          </Grid2>
+          <Grid2 item xs={12} md={4}>
+            <Typography variant="h6">Smart Flashcards</Typography>
+            <Typography>
+              {' '}
+              Our AI intelligently breaks down your text into concise
+              flashcards, perfect for studying
+            </Typography>
+          </Grid2>
+          <Grid2 item xs={12} md={4}>
+            <Typography variant="h6">Accessible Anywhere</Typography>
+            <Typography>
+              {' '}
+              Access your flashcards from any device, at any time. Study
+              on the go with ease.
+            </Typography>
+          </Grid2>
+        </Grid2>
+      </Box>
+      <Box sx={{ my: 6, textAlign: 'center' }}>
+        <Typography variant="h4">Pricing</Typography>
+        <Grid2 container spacing={4}>
+          <Grid2 item xs={12} md={4}>
+            <Box
+              sx={{
+                p: 3,
+                border: '1px solid',
+                borderColor: 'grey.300',
+                borderRadius: 2,
+              }}
+            >
+              <Typography variant="h5" gutterBottom>
+                Basic
+              </Typography>
+              <Typography variant="h6" gutterBottom>
+                $5 / month
+              </Typography>
+              <Typography>
+                {' '}
+                Access to basic flashcard features and limited storage.
+              </Typography>
+              <Button variant="contained" color="primary">
+                Choose Basic
+              </Button>
+            </Box>
+          </Grid2>
+          <Grid2 item xs={12} md={4}>
+            <Typography variant="h6">Smart Flashcards</Typography>
+            <Typography>
+              {' '}
+              Our AI intelligently breaks down your text into concise
+              flashcards, perfect for studying
+            </Typography>
+          </Grid2>
+          <Grid2 item xs={12} md={4}>
+            <Typography variant="h6">Accessible Anywhere</Typography>
+            <Typography>
+              {' '}
+              Access your flashcards from any device, at any time. Study
+              on the go with ease.
+            </Typography>
+          </Grid2>
+        </Grid2>
+      </Box>
     </Container>
   )
 }
